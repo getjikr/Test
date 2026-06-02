829a4fac49c1ae1d14f44f4db8bf56b0d155f530
getji <getji@khu.ac.kr>
2026-06-02 23:04:38 +0900

    feat: 플라이 속도 변경 명령어 추가

--- src/main/java/io/github/getjikr/test/Test.java
+++ src/main/java/io/github/getjikr/test/Test.java
@@ -1,6 +1,7 @@
 package io.github.getjikr.test;
 
 import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.FloatArgumentType;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.tree.LiteralCommandNode;
@@ -13,14 +14,18 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.NullMarked;
 
+@NullMarked
 public final class Test extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("PO");
-        this.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, commands ->
-                commands.registrar().register(this.giveDiamondCommand()));
+        this.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, commands -> {
+            commands.registrar().register(this.giveDiamondCommand());
+            commands.registrar().register(this.setFlySpeedCommand());
+        });
+
     }
 
     private LiteralCommandNode<CommandSourceStack> giveDiamondCommand() {
@@ -30,7 +35,24 @@ public final class Test extends JavaPlugin {
                 ).build();
     }
 
-    private int giveDiamondLogic(@NonNull CommandContext<CommandSourceStack> ctx) {
+    private LiteralCommandNode<CommandSourceStack> setFlySpeedCommand() {
+        return Commands.literal("flyspeed")
+                .then(Commands.argument("speed", FloatArgumentType.floatArg(0,1.0f))
+                        .executes(ctx -> {
+                            if (!(ctx.getSource().getExecutor() instanceof Player player)) {
+
+                                return Command.SINGLE_SUCCESS;
+                            }
+                            float speed = ctx.getArgument("speed", Float.class);
+                            player.setFlySpeed(speed);
+                            player.sendMessage(Component.text("플레이어의 플라이 속도가 " + speed + "로 설정되었습니다.")
+                                    .color(NamedTextColor.GOLD));
+                            return Command.SINGLE_SUCCESS;
+                        })
+                ).build();
+    }
+
+    private int giveDiamondLogic(CommandContext<CommandSourceStack> ctx) {
         if (ctx.getSource().getExecutor() instanceof Player player) {
             int amount = ctx.getArgument("amount", Integer.class);
             ItemStack diamonds = new ItemStack(Material.DIAMOND, amount);
